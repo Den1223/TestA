50ff62aa6f08751ab1d333fdcbc07d0878619281
Den1223 <123265092+Den1223@users.noreply.github.com>
2026-01-05 18:58:26 +0200

    Update 0.2 (create UI)

--- TestA/ViewsModels/MainViewsModel.cs
+++ TestA/ViewsModels/MainViewsModel.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
+
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace TestA.ViewsModels
 {
@@ -12,6 +11,63 @@ namespace TestA.ViewsModels
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        private string _code;
+        private string _selectLanguage;
+        private string _status;
+        private string _output;
+        private int _timeLimit;
+        private int _meomoryLimit;
+
+        public ObservableCollection<string> Languages { get; } =
+            new ObservableCollection<string>
+            {
+                "C#",
+                "Python",
+                "JavaScript"
+            };
+        public string Code
+        {
+            get => _code;
+            set { _code = value; OnPropertyChanged(); }
+        }
+
+        public string SelectedLanguage
+        {
+            get => _selectLanguage;
+            set { _selectLanguage = value; OnPropertyChanged(); }
+        }
+        public int TimeLimit
+        {
+            get => _timeLimit;
+            set { _timeLimit = value; OnPropertyChanged(); }
+        }
+
+        public int MemoryLimit
+        {
+            get => _meomoryLimit;
+            set { _meomoryLimit = value; OnPropertyChanged(); }
+        }
+
+        public string Status
+        {
+            get => _status;
+            set { _status = value; OnPropertyChanged(); }
+        }
+
+        public string Output
+        {
+            get => _output;
+            set { _output = value; OnPropertyChanged(); }
+        }
+
+        public MainViewsModel()
+        {
+            SelectedLanguage = "C#";
+            TimeLimit = 2;
+            MemoryLimit = 64;
+            Status = "Idle";
+            Code = "Write your code here /n";
+        }
         protected void OnPropertyChanged([CallerMemberName] string? name = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
